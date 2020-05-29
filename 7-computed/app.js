bb053d2886cbf599eadfca203309e24345d83bf8
Tamas Kuti <tamas.kuti@oetkerdigital.com>
2020-05-29 10:15:44 +0200

    classes and attribs

---
+++ 7-computed/app.js
@@ -0,0 +1,63 @@
+class ShoppingListItem {
+    label = "";
+    purchased = false;
+    highPriority = false;
+
+    constructor(label) {
+        this.label = label;
+    }
+}
+
+const makeShoppingListItem = label=> {
+    return new ShoppingListItem(label);
+};
+
+const dummyItemLabels = ["Sör", "Bor", "Pálinka"];
+
+const makeInitialItems = (labels)=> {
+    return labels.map(label=> makeShoppingListItem(label));
+};
+
+const shoppingList = new Vue({
+    el: "#shopping-list", // works with DOMElement too so document.getElementById("foo") or document.querySelector("#foo") is the same
+    data: {
+        header: "Bevásárló lista",
+        newItem: "",
+        items: makeInitialItems(dummyItemLabels),
+        state: "default"
+    },
+    computed: {
+        reversedItems() {
+            return [...this.items].reverse();
+        },
+        sortedItems() {
+            return [...this.items].sort((a, b)=> {
+                return a.label.localeCompare(b.label);
+            });
+        }
+    },
+    methods: {
+        clearInput() {
+            this.newItem = "";
+        },
+        focusInput() {
+            this.$refs.itemInput.focus();
+        },
+        addItem() {
+            const item = makeShoppingListItem(this.newItem);
+
+            shoppingList.$data.items = [...shoppingList.$data.items, ...[item]];
+            this.clearInput();
+            this.focusInput();
+
+            return shoppingList.$data.items;
+        },
+        changeState(newState) {
+            this.state = newState;
+            this.clearInput();
+        },
+        togglePurchased(item) {
+            item.purchased = !item.purchased;
+        }
+    }
+});
